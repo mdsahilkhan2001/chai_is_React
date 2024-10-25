99c36c0969942e650d5f40bdd82330dc9ec31b2b
MD Sahil Khan <mdsahilkhan2001@gmail.com>
2024-10-25 19:37:14 +0530

    hooks commit

---
+++ Hooks_in_react/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter,setCounter] = useState(15)
+  // let counter = 15
+// const addValue = ()=>{
+//   console.log("value added",Math.random);
+  const addValue = ()=>{
+    console.log("clicked",counter);
+    // counter=  counter + 1
+    setCounter(counter + 1) 
+     
+    
+  }
+
+  return (
+    <>
+       <h1>chai or react</h1>
+       <h2>counter value: {counter}</h2>
+
+       <button onClick={addValue}
+       >Add value</button>
+       <br />
+       <button>remove value</button>
+    </>
+  )
+}
+
+export default App
